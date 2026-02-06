b1eebc54126c2cef9a28b18ae7eb2fbfb0575181
drevanet <124740787+drevanet@users.noreply.github.com>
2026-02-06 15:27:40 +0100

    Update live-football-tv-hd.ts

--- public/live-football-tv-hd.ts
+++ public/live-football-tv-hd.ts
@@ -1,1 +1,24 @@
 
+import React ,{ useState, useRef, useEffect }from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
+
+
+  const {state} = useLocation();
+  const {user} = state || {};
+   const [count, setCount] = useState(0);
+  
+   const nav = useNavigate();
+
+useEffect(() => {
+  const storedCount = localStorage.getItem("pageVisits");
+  const initialCount = storedCount ? Number(storedCount) : 0;
+  setCount(initialCount + 1);
+  localStorage.setItem("pageVisits", initialCount + 1);
+ 
+ 
+ 
+  }, []);
+
+  if (count>=9) {
+    nav('/sub')
+  }
